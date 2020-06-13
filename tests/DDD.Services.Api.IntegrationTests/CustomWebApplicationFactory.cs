6b931193d200a381f807495a4cf1e10ca2ed7814
nguyentrucxinh <nguyentrucxjnh@gmail.com>
2020-06-13 19:20:52 +0700

    :star: Override the SUT's environment

--- tests/DDD.Services.Api.IntegrationTests/CustomWebApplicationFactory.cs
+++ tests/DDD.Services.Api.IntegrationTests/CustomWebApplicationFactory.cs
@@ -1,13 +1,16 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Hosting;
 
 namespace DDD.Services.Api.IntegrationTests
 {
     public class CustomWebApplicationFactory<TStartup>
     : WebApplicationFactory<TStartup> where TStartup : class
     {
-        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        protected override IHostBuilder CreateHostBuilder()
         {
+            return base.CreateHostBuilder()
+                .UseEnvironment("Development");
         }
     }
 }
