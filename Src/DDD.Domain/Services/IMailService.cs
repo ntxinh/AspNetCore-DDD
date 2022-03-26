d06906472bd5d9b56780f01f90c08ca9428a738f
Xinh Nguyen <nguyentrucxjnh@gmail.com>
2022-03-26 19:02:50 +0700

    Update IMailService.cs

--- Src/DDD.Domain/Services/IMailService.cs
+++ Src/DDD.Domain/Services/IMailService.cs
@@ -8,23 +8,24 @@ namespace DDD.Domain.Services
     public interface IMailService
     {
         void SendMail(MailMessage mailMessage);
+//         Task Send(MailMessage message);
     }
     
-    public class MailMessage
-    {
-        public MailAddress From { get; set; }
-        public List<MailAddress> To { get; set; }
-        public List<MailAddress> Cc { get; set; }
-        public List<MailAddress> Bcc { get; set; }
-        public string Subject { get; set; }
-        public string PlainTextContent { get; set; }
-        public string TemplateId { get; set; }
-        public object DynamicTemplateData { get; set; }
-    }
+//     public class MailMessage
+//     {
+//         public MailAddress From { get; set; }
+//         public List<MailAddress> To { get; set; }
+//         public List<MailAddress> Cc { get; set; }
+//         public List<MailAddress> Bcc { get; set; }
+//         public string Subject { get; set; }
+//         public string PlainTextContent { get; set; }
+//         public string TemplateId { get; set; }
+//         public object DynamicTemplateData { get; set; }
+//     }
 
-    public class MailAddress
-    {
-        public string Email { get; set; }
-        public string Name { get; set; }
-    }
+//     public class MailAddress
+//     {
+//         public string Email { get; set; }
+//         public string Name { get; set; }
+//     }
 }
