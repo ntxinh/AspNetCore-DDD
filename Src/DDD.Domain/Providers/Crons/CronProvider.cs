b329378450ced0f1b9988e3a271835412677e0cc
ntxinh <nguyentrucxjnh@gmail.com>
2023-12-03 09:38:09 +0700

    :star: Update CronProvider

--- Src/DDD.Domain/Providers/Crons/CronProvider.cs
+++ Src/DDD.Domain/Providers/Crons/CronProvider.cs
@@ -12,25 +12,51 @@ public class CronProvider : ICronProvider
         _schedulerFactory = schedulerFactory;
     }
 
-    public async Task NotifyInactiveUser(NotifyInactiveUserConsumerModel payload)
+    // public async Task NotifyInactiveUser(NotifyInactiveUserConsumerModel payload)
+    // {
+    //     IScheduler sched = await _schedulerFactory.GetScheduler();
+
+    //     IDictionary<string, object> jobData = new Dictionary<string, object>
+    //     {
+    //         { nameof(NotifyInactiveUserConsumerModel.Data), payload.Data },
+    //         { nameof(NotifyInactiveUserConsumerModel.TenantId), payload.TenantId },
+    //         { nameof(NotifyInactiveUserConsumerModel.UserId), payload.UserId },
+    //     };
+
+    //     var job = JobBuilder.Create<NotifyInactiveUserJob>()
+    //         .WithIdentity(nameof(NotifyInactiveUserJob))
+    //         .Build();
+
+    //     var replace = true;
+    //     var durable = true;
+    //     await sched.AddJob(job, replace, durable);
+
+    //     await sched.TriggerJob(new JobKey(nameof(NotifyInactiveUserJob)), new JobDataMap(jobData));
+    // }
+
+    public async Task OneOffJob<T>(IDictionary<string, object> jobData) where T : IJob
     {
         IScheduler sched = await _schedulerFactory.GetScheduler();
 
-        IDictionary<string, object> jobData = new Dictionary<string, object>
-        {
-            { nameof(NotifyInactiveUserConsumerModel.Data), payload.Data },
-            { nameof(NotifyInactiveUserConsumerModel.TenantId), payload.TenantId },
-            { nameof(NotifyInactiveUserConsumerModel.UserId), payload.UserId },
-        };
+        var jobKey = typeof(T).Name;
 
-        var job = JobBuilder.Create<NotifyInactiveUserJob>()
-            .WithIdentity(nameof(NotifyInactiveUserJob))
+        var job = JobBuilder.Create<T>()
+            .WithIdentity(jobKey)
             .Build();
 
         var replace = true;
         var durable = true;
         await sched.AddJob(job, replace, durable);
 
-        await sched.TriggerJob(new JobKey(nameof(NotifyInactiveUserJob)), new JobDataMap(jobData));
+        await sched.TriggerJob(new JobKey(jobKey), new JobDataMap(jobData));
+
+        // How to use
+        // var jobData = new Dictionary<string, object>
+        // {
+        //     { nameof(NotifyInactiveUserConsumerModel.Data), payload.Data },
+        //     { nameof(NotifyInactiveUserConsumerModel.TenantId), payload.TenantId },
+        //     { nameof(NotifyInactiveUserConsumerModel.UserId), payload.UserId },
+        // };
+        // await _quartzProvider.OneOffJob<NotifyInactiveUserJob>(jobData);
     }
 }
